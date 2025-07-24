05a05633f7523c830d43000da6c27dd5a7cea358
PavlomirDoitchev <pavlomirdoitchev@gmail.com>
2025-07-24 23:07:58 +0300

    Adding products

--- AgroShopApp.Web.ViewModels/AllProductsViewModel.cs
+++ AgroShopApp.Web.ViewModels/AllProductsViewModel.cs
@@ -4,6 +4,16 @@ namespace AgroShopApp.Web.ViewModels
 {
     public class AllProductsViewModel
     {
+        public Guid Id { get; set; }
 
+        public string Name { get; set; } = null!;
+
+        public string Description { get; set; } = null!;
+
+        public decimal Price { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public string Category { get; set; } = null!;
     }
 }
--- AgroShopApp.Web/Controllers/ProductController.cs
+++ AgroShopApp.Web/Controllers/ProductController.cs
@@ -1,5 +1,7 @@
 ﻿using AgroShopApp.Data;
+using AgroShopApp.Web.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Runtime.CompilerServices;
 
 namespace AgroShopApp.Web.Controllers
@@ -12,9 +14,31 @@ namespace AgroShopApp.Web.Controllers
         {
             _context = context;
         }
-        public IActionResult Index()
+        public async Task<IActionResult> Index(int? categoryId = null)
         {
-            return View();
+            var productsQuery = _context.Products
+                .AsNoTracking()
+                .Include(p => p.Category)
+                .Where(p => p.IsAvailable);
+
+            if (categoryId.HasValue)
+            {
+                productsQuery = productsQuery.Where(p => p.CategoryId == categoryId);
+            }
+
+            var model = await productsQuery
+                .Select(p => new AllProductsViewModel
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Description = p.Description,
+                    Price = p.Price,
+                    ImageUrl = p.ImageUrl,
+                    Category = p.Category.Name
+                })
+                .ToListAsync();
+
+            return View(model);
         }
     }
 }
