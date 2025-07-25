788dba12789cb0403de81d701a6e066f4477a972
PavlomirDoitchev <pavlomirdoitchev@gmail.com>
2025-07-25 16:29:41 +0300

    Updating UX

---
+++ AgroShopApp.Web.Infrastructure/Extensions/StringExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AgroShopApp.Web.Infrastructure.Extensions
+{
+    public static class StringExtensions
+    {
+        public static string Truncate(this string input, int length = 100)
+        {
+            if (string.IsNullOrEmpty(input)) return input;
+            return input.Length > length ? input.Substring(0, length) + "..." : input;
+        }
+    }
+}
