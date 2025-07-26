ad00503881249f4246acefe3b580a567baaf8650
PavlomirDoitchev <pavlomirdoitchev@gmail.com>
2025-07-26 10:34:35 +0300

    UI improvements

--- AgroShopApp.GCommon/ApplicationConstants.cs
+++ AgroShopApp.GCommon/ApplicationConstants.cs
@@ -3,6 +3,7 @@
     public static class ApplicationConstants
     {
         public const string AppDateFormat = "yyyy-MM-dd";
+        public const string DeletedOnFormat = "yyyy-MM-dd HH:mm";
         public const string NoImageUrl = "no-image.jpg";
         public const string IsDeletedPropertyName = "IsDeleted";
         public const string PriceSqlType = "decimal(18, 6)";
