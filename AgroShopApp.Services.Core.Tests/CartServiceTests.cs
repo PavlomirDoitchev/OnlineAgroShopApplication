98238c6f132422e847c4d6321efef301e2aa73eb
PavlomirDoitchev <pavlomirdoitchev@gmail.com>
2025-08-05 20:35:54 +0300

    Updating project packages to allow for testing the app's functionality.

---
+++ AgroShopApp.Services.Core.Tests/CartServiceTests.cs
@@ -0,0 +1,19 @@
+﻿namespace AgroShopApp.Services.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Moq;
+    using NUnit.Framework;
+    using AgroShopApp.Data.Models;
+    using AgroShopApp.Data.Repository.Contracts;
+    using AgroShopApp.Services.Core;
+    using AgroShopApp.Web.ViewModels.Cart;
+
+    [TestFixture]
+    public class CartServiceTests
+    {
+        
+    }
+}
